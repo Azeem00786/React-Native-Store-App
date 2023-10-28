70c1f2d5d16b9fbf59100eb80a4b6a6a6901b4de
Mohd_Azeem <azeemmohammad377@gmail.com>
2023-10-28 11:40:23 +0530

    add voice

--- App.js
+++ App.js
@@ -7,6 +7,10 @@ import CategoryDetail from './screens/CategoryDetail';
 import { Component } from 'react';
 import codePush from "react-native-code-push";
 
+import {NativeModules} from 'react-native';
+
+const {VoiceChangingModule} = NativeModules;
+
 import {
   StyleSheet,
   View,
@@ -30,14 +34,67 @@ class App extends Component {
         installMode: codePush.InstallMode.IMMEDIATE
     });
 }
+callJavaMethod (){
+        alert('hii')
+}
+ changeToChild  ()  {
+  console.log('changeToChild');
+  var audioTrackURL = 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3';
+  Platform.OS === 'android' && VoiceChangingModule.changeVoiceToChild(audioTrackURL);
+};
+
+changeVoiceToAlien  ()  {
+  console.log('changeToChild');
+  var audioTrackURL = 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3';
+  Platform.OS === 'android' && VoiceChangingModule.changeVoiceToChild(audioTrackURL);
+};
+
+speedUpVoice  ()  {
+  console.log('changeToChild');
+  var audioTrackURL = 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3';
+  Platform.OS === 'android' && VoiceChangingModule.changeVoiceToChild(audioTrackURL);
+};
+
+slowDownVoice  ()  {
+  console.log('changeToChild');
+  var audioTrackURL = 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3';
+  Platform.OS === 'android' && VoiceChangingModule.changeVoiceToChild(audioTrackURL);
+};
+
+stopRing()  {
+  var audioTrackURL = 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3';
+  console.log('stopRing');
+  VoiceChangingModule.stopSound();
+};
+justTest(){
+  console.log('juatTest')
+}
   render() {
     return (
-            <View>
+            <View style={styles.mastyleLoginBtnin}>
+              <Text>hellow</Text>
+              <View style={styles.button}>
+              <Button title='change Voice To Alien' onPress={this.justTest} />
+            </View> 
+              {/* <Button title='child voice' onPress={this.changeToChild}/>
+            <View style={styles.button}>
+              <Button title='changeVoiceToAlien' onPress={this.changeVoiceToAlien} />
+            </View> 
+            <View style={styles.button}>
+              <Button title='speedUpVoice' onPress={this.speedUpVoice} />
+            </View>
+            <View style={styles.button}>
+              <Button title='slowDownVoice' onPress={this.slowDownVoice} />
+            </View> 
+             
+            <View style={styles.button}>
+              <Button title='stop sound' onPress={this.stopRing} />
+            </View> 
                 <TouchableOpacity onPress={this.onButtonPress}>
                     <Text style={{color:'black', textAlign:'center'}}>Check for updates</Text>
                     <Text style={{color:'black', textAlign:'center'}}>Check for updates code push</Text>
                     <Text style={{color:'black', textAlign:'center'}}>Check for updates withoout update</Text>
-                </TouchableOpacity>
+                </TouchableOpacity> */}
             </View>
            
     //  <SafeAreaView style={{flex:1, backgroundColor:'hite'}}>
@@ -54,8 +111,14 @@ class App extends Component {
 };
 
 const styles = StyleSheet.create({
- main:{
-  
+ mastyleLoginBtnin:{
+  padding:10,
+  margin:10,
+  height:100,
+  width:400
+ },
+ button:{
+  paddingTop:10
  }
 });
 App = codePush(codePushOptions)(App)
